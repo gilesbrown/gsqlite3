091ef58b750cc3b0ea3f7ef8be05ca253b1b5a74
Giles Brown <gsbrown@ebay.com>
2014-10-29 19:29:53 -0700

    Fixed setup.py and bumped patch version

--- README.rst
+++ README.rst
@@ -1,4 +1,12 @@
 gsqlite3
-======== 
+========
 
-A gevent style wrapper for pysqlite3, including a SQLAlchemy dialect.
+A gevent-ification of pysqlite3, including a SQLAlchemy dialect.
+
+The module takes a simple approach of any potentiall long running methods off 
+to the gevent hub threadpool for execution.
+
+
+SQLAlchemy
+----------
+You can use gsqlite3 with SQLAlchemy by specifying 'sqlite+gsqlite3' as your URL scheme.
--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
+version = '0.1.1'
+
 setup(
     name='gsqlite3',
     description="A wrapper for 'sqlite3' to make it play better with 'gevent'",
-    version='0.1',
+    version=version,
     author='Giles Brown',
     author_email='giles_brown@hotmail.com',
     url='http://github.com/gilesbrown/gsqlite3',
-    download_url = 'http://github.com/gilesbrown/gsqlite3/tarball/' + __version__,
+    download_url = 'http://github.com/gilesbrown/gsqlite3/tarball/' + version,
     license='BSD',
-    scripts=['gsqlite3.py'],
+    py_modules=['gsqlite3'],
     install_requires=['gevent>=1.0.1'],
     classifiers=(
         'Development Status :: 3 - Alpha',
